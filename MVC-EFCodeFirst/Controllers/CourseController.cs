ebd34260bf8fbe4387cbfbcf7c62ce69595be6c9
Anamika-s <anamika.sawhney22@gmail.com>
2024-01-23 13:16:00 +0530

    EF Core added

--- MVC-EFCodeFirst/Controllers/CourseController.cs
+++ MVC-EFCodeFirst/Controllers/CourseController.cs
@@ -1,13 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_EFCodeFirst.Context;
+using MVC_EFCodeFirst.Models;
 
 namespace MVC_EFCodeFirst.Controllers
 {
     public class CourseController : Controller
     {
+        CourseDbContext _context;
+        public CourseController(CourseDbContext context)
+        {
+            _context = context; 
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var list = _context.Courses.ToList();
+            if (list.Count == 0)
+            {
+                ViewBag.msg = "No rec";
+                return View();
+            }
+            else
+            {
+                return View(_context.Courses.ToList());
+            }
         }
+
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Create(Course course)
+        {
+
+            _context.Courses.Add(course);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int? id)
+        {
+            return View(_context.Courses.Where(c => c.Id == id).FirstOrDefault());   
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            return View(_context.Courses.Where(c => c.Id == id).FirstOrDefault());
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Course course)
+        {
+            var temp = _context.Courses.FirstOrDefault(x=>x.Id==course.Id);
+            if (temp != null)
+            {
+                temp = course;
+                _context.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+
+        }
+        
     }
 }
